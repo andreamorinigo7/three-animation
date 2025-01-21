6f8677b65e26a26c1c175dba0e6edcfbf13e0b16
Andrea <morinigoandrea7@gmail.com>
2025-01-21 15:31:57 -0500

    square shape rendered

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Three from "./Three";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Three />
+      </header>
+    </div>
+  );
+}
+
+export default App;
