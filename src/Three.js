4ae37538024dec4d4e37edde6b402f4d30e2535e
Andrea <morinigoandrea7@gmail.com>
2025-01-21 17:03:36 -0500

    cursor movement enabled

--- src/Three.js
+++ src/Three.js
@@ -5,6 +5,8 @@ export default function Three() {
   console.log(THREE);
   const refContainer = useRef(null);
   useEffect(() => {
+    const cursor = { x: 0, y: 0 };
+
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(
       55,
@@ -27,11 +29,29 @@ export default function Three() {
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
 
+    window.addEventListener("resize", () => {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    });
+
+    window.addEventListener("mousemove", (event) => {
+      cursor.x = event.clientX / window.innerWidth - 0.5;
+      cursor.y = event.clientY / window.innerHeight - 0.5;
+    });
+
     const animate = function () {
       window.requestAnimationFrame(animate);
 
       mesh.rotation.y += 0.01;
       mesh.rotation.x += 0.01;
+
+      const cameraX = cursor.x;
+      const cameraY = cursor.y;
+
+      camera.position.x += (cameraX - camera.position.x) / 10;
+      camera.position.y += (cameraY - camera.position.y) / 10;
 
       renderer.render(scene, camera);
     };
